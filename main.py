24d8dd595ce58fed4c4122623d7a67d0a196e572
whoami847 <mrbeastbangla069@gmail.com>
2025-04-10 06:31:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from pyrogram import Client, filters
-from flask import Flask, request
 import os
+from flask import Flask, request
+from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+WEBHOOK_URL = "https://anime-lord.koyeb.app"
+
 app = Flask(__name__)
 bot = Client("anime_lord", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-WEBHOOK_URL = "https://anime-lord.koyeb.app"
 
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def webhook():
@@ -17,15 +17,11 @@ def webhook():
     return "OK"
 
 @app.route("/", methods=["GET"])
-def root():
-    return "Bot Running (Webhook Mode)"
+def home():
+    return "Bot is Running with Webhook"
 
 if __name__ == "__main__":
     import uvicorn
-
-    # Telegram এর webhook সেট করা
     with bot:
         bot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
-    
-    # Flask app চালানো
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
